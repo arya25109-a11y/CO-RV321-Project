8988e97105a4ba7f93bbf40a0b89b8596ef6cd44
Aarav Mathur <aaravmathur@Aaravs-MacBook-Air.local>
2026-03-04 22:31:19 +0530

    meow

--- assembler/assembler.c
+++ assembler/assembler.c
@@ -1,10 +1,7 @@
 # include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc,char *argv[]){
-	if(argc!=3){
-		printf("Usage %s <input_files><out>"
-	}
+int main(){
 	printf("hello charlie!");
 }
 
